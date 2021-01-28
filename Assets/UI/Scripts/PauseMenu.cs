7ef4da4d46f410597d4203626bad98780cf763d0
mostafa-elhaiany <mostafa_elhaiany@outlook.com>
2021-01-28 20:29:35 +0200

    Pause Menu
    UI and pause logic

---
+++ Assets/UI/Scripts/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public RawImage menu;
+    public Button resume;
+    public Button restart;
+    public Button mainMenu;
+
+    void Start()
+    {
+        menu.gameObject.SetActive(false);
+        resume.onClick.AddListener(resumeOnClick);
+        restart.onClick.AddListener(restartOnClick);
+        mainMenu.onClick.AddListener(menuOnClick);
+    }
+
+    void resumeOnClick()
+    {
+        GameManager.isPaused = false;
+    }
+
+    void restartOnClick()
+    {
+        //TODO add scene manager
+    }
+
+    void menuOnClick()
+    {
+        //TODO add scene manager
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            GameManager.isPaused = !GameManager.isPaused;
+        }
+
+        menu.gameObject.SetActive(GameManager.isPaused);
+    }
+}
