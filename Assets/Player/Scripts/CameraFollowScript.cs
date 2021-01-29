37de51ed6454dcf9c36057e3ef12de8a9bba69eb
mostafa-elhaiany <mostafa_elhaiany@outlook.com>
2021-01-29 18:40:36 +0200

    Update CameraFollowScript.cs

--- Assets/Player/Scripts/CameraFollowScript.cs
+++ Assets/Player/Scripts/CameraFollowScript.cs
@@ -8,15 +8,8 @@ public class CameraFollowScript : MonoBehaviour
     public Transform player;
     public float rotateSpeed;
 
-    private void Start()
-    {
-        //Cursor.visible = false;
-        //Cursor.lockState = CursorLockMode.Locked;
-    }
-
     void LateUpdate()
     {
-        //transform.Rotate(-1 * Input.GetAxis("Mouse Y") * rotateSpeed * Time.deltaTime, 0, 0);
         float xRotation = Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime;
         float yRotation = -Input.GetAxis("Mouse Y") * rotateSpeed * Time.deltaTime;
         if (!Input.GetKey(KeyCode.LeftAlt))
@@ -26,21 +19,14 @@ public class CameraFollowScript : MonoBehaviour
             this.transform.Rotate(yRotation, 0, 0);
 
             Quaternion rot = this.transform.rotation;
-            float eulerX = rot.eulerAngles.x;//Mathf.Clamp(rot.eulerAngles.x, 25, 0);
-            rot = Quaternion.Euler(eulerX, rot.eulerAngles.y, rot.eulerAngles.z);
-            Debug.Log(rot.eulerAngles);
-
-
-            //transform.rotation = Quaternion.Euler(Mathf.Clamp(transform.rotation.eulerAngles.x, -45, 45), 4, 0);
-
-
-
-            //this.transform.rotation = Quaternion.Euler(0, 0, 0);
+            float eulerX = Mathf.Clamp(rot.eulerAngles.x, 5, 30);
+            rot = Quaternion.Euler(eulerX, transform.localEulerAngles.y, transform.localEulerAngles.z);
+            transform.localRotation = rot;
+            
         }
         else
         {
             cameraLookCube.transform.Rotate(yRotation, xRotation, 0);
-            //this.transform.Rotate(yRotation, 0, 0);
         }
 
     }
