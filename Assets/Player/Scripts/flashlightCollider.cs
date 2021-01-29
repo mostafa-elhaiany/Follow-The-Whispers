5480e0b912ab0fdacf7d662f275225f1c93ee03c
mostafa-elhaiany <mostafa_elhaiany@outlook.com>
2021-01-29 15:41:45 +0200

    added player prefab
    Player prefab
    Flashlight collider

---
+++ Assets/Player/Scripts/flashlightCollider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flashlightCollider : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+
+        //if(other.CompareTag("Ghost"))
+        //{
+
+        //}
+        //else if (other.CompareTag("Nanny"))
+        //{
+
+        //}
+    }
+}
