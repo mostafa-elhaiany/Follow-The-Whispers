e37f3f052e2c37412e4a662060093d4d32b7bbdb
mostafa-elhaiany <mostafa_elhaiany@outlook.com>
2021-01-30 20:02:30 +0200

    added theme in main menu updated mixer

--- Assets/UI/Scripts/MainMenu.cs
+++ Assets/UI/Scripts/MainMenu.cs
@@ -6,17 +6,22 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
     // Start is called before the first frame update
     public Button start;
     Button howToPlay;
     Button credits;
     Button quit;
+    public GameObject loading;     
     public string gameSceneName;
     public string creditsSceneName;
     public string howToPlaySceneName;
     
     void Start()
     {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+
         start = GameObject.Find("Start").GetComponent<Button>();
         howToPlay = GameObject.Find("HowTo").GetComponent<Button>();
         credits = GameObject.Find("Credits").GetComponent<Button>();
@@ -31,12 +36,25 @@ public class MainMenu : MonoBehaviour
         creditsSceneName = "Credits";
         howToPlaySceneName = "HowToPlay";
 
-        
+        loading.SetActive(false);
+        start.gameObject.SetActive(true);
+        howToPlay.gameObject.SetActive(true);
+        credits.gameObject.SetActive(true);
+        quit.gameObject.SetActive(true);
+
+
+
     }
 
     public void StartGame(){
         //Debug.Log("CLICKED");
         //TODO: UNCOMMENT
+        loading.SetActive(true);
+        start.gameObject.SetActive(false);
+        howToPlay.gameObject.SetActive(false);
+        credits.gameObject.SetActive(false);
+        quit.gameObject.SetActive(false);
+
         SceneManager.LoadScene(gameSceneName);
 		
 	}
