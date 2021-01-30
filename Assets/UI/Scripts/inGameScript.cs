6b1f8213df55db4a55e348b2db9a695b8f34536a
zainab <zainabsultan1111@gmail.com>
2021-01-30 10:54:18 +0200

    sister + ui

---
+++ Assets/UI/Scripts/inGameScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class inGameScript : MonoBehaviour
+{
+
+    public Canvas canvas;
+    public GameObject panel;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Z)){
+            panel.SetActive(!panel.activeSelf);
+        }
+
+        
+    }
+}
