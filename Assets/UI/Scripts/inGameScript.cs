ca8642baac2b45c0dcb9b85018869b5444c3a380
mostafa-elhaiany <mostafa_elhaiany@outlook.com>
2021-01-30 17:19:01 +0200

    ui integrations pt2

--- Assets/UI/Scripts/inGameScript.cs
+++ Assets/UI/Scripts/inGameScript.cs
@@ -8,8 +8,15 @@ public class inGameScript : MonoBehaviour
     public Canvas canvas;
     public GameObject panel;
     // Start is called before the first frame update
+    public RawImage[] keysImages;
+    public string keyName;
     void Start()
     {
+        for(int i=0; i<keysImages.Length;i++){
+            //set all keys to false
+           keysImages[i].enabled=false;
+
+        }
        
 
         
@@ -21,7 +28,24 @@ public class inGameScript : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Z)){
             panel.SetActive(!panel.activeSelf);
         }
+        //if(Input.GetKeyDown(KeyCode.KeypadEnter)){
+            keyFound();
+        //}
+
 
         
+    }
+    public void keyFound(){
+
+        for(int i=0; i<keysImages.Length;i++){
+            //get the image u need
+            //Debug.Log(keysImages[i].GetComponent<RawImage>().texture.name);
+            if(keyName.Contains(keysImages[i].GetComponent<RawImage>().texture.name) ){
+                keysImages[i].enabled=true;
+            }
+
+        }
+
+
     }
 }
